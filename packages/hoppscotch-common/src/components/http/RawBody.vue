ede641d8892110f4ddc55d242601543b8b497048
gujie <704572066@qq.com>
2024-01-16 14:00:41 +0800

    RawBody.vue 添加注释

--- packages/hoppscotch-common/src/components/http/RawBody.vue
+++ packages/hoppscotch-common/src/components/http/RawBody.vue
@@ -42,6 +42,9 @@
             @click="$refs.payload.click()"
           />
         </label>
+        <!-- 建议不要通过click绑定事件，对待input标签最好通过change来触发，change绑定的事件是一定要等到input框的value值改变之后才会被触发。
+        click执行的时间要早于change执行的时间，因为v-modal的时间是一个异步的。
+        当点击之后，v-modal可能还没有来得及将绑定在data里面的数据改变，click绑定的事件就执行了，这会导致click绑定事件里面拿到的data数据不是最新的。-->
         <input
           ref="payload"
           class="input"
@@ -144,14 +147,44 @@ useCodemirror(
 const clearContent = () => {
   rawParamsBody.value = ""
 }
-
+// TaskOption<A> represents an asynchronous computation that, when run, will produce a value of type Option<A>. 
+// In simpler terms, it's an operation that, once completed, might give you a result (some(value)) or might tell you that there's no result (none), and this entire process is asynchronous.
+// export interface TaskOption<A> extends Task<Option<A>> {}
 const uploadPayload = async (e: InputEvent) => {
   await pipe(
+    // 如果我们使用可选链接 ?，files变量不存在它将返回 undefined 作为输出。
     (e.target as HTMLInputElement).files?.[0],
     TO.of,
+    // When calling predicate with value returns true, then running TaskOption returns Some(value). Otherwise return None.
+    /* 。
+       在 TypeScript 中，is是一个 类型谓词，用来帮助 ts 编译器 收窄 变量的类型。
+       具体场景是，我们需要通过if来判断某个变量的类型，从而针对特定的类型执行一些特定的操作。
+       当然对于一些内置的判断方式，例如typeof v === 'number'，ts 已经默认支持了类型的自动收窄：
+       function test(v: number | undefined | null) {
+          if (typeof v === 'number') {
+              console.log(Math.round(v))
+          }
+       }
+       上述代码 ts 不会报错，并且能观察到变量的类型被收窄了
+
+       function isString(s:unknown):s is string
+       {
+          return typeof s === 'string'
+       }
+       // 判断参数是否为字符串,是在调用转大写方法
+       function ifUpperCase(str:unknown)
+       {
+          if(isstring(str)){
+            str.toUpperCase()
+            // (parameter) str: string 
+          }
+       }
+
+       平常对某value的用as断言为某种类型，但是as会出现不安全的问题，如果使用is对类型进行收窄，对代码的安全性更高
+    */
     TO.chain(TO.fromPredicate((f): f is File => f !== undefined)),
     TO.chain(readFileAsText),
-
+    // export declare const matchW: <B, A, C>(onNone: () => B, onSome: (a: A) => C) => (ma: TaskOption<A>) => T.Task<B | C>
     TO.matchW(
       () => toast.error(`${t("action.choose_file")}`),
       (result) => {
